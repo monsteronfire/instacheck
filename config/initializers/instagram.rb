ded0df81f6bbb73bd92eae8a63ac6cd0214ea69b
monsteronfire <monsteronfire@gmail.com>
2015-10-30 15:32:38 +0800

    adds instagram app shit

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,6 @@
 require "instagram"
 
 Instagram.configure do |config|
-    config.client_id = ""
-    config.access_token = ""
+    config.client_id = "8d0c4adf568146c7968559a9a7ee4d2f"
+    config.access_token = "80a93683b3d84490944f0631f910ff77"
 end
