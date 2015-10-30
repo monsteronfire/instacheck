5456eac05d2db0a8b4120390ff436d09381f0a86
monsteronfire <monsteronfire@gmail.com>
2015-10-31 01:30:32 +0800

    adds list of user follows on index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @popular = Instagram.media_popular
+    @recent = Instagram.user_follows
   end
 end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -4,5 +4,6 @@ CALLBACK_URL = "http://localhost/"
 
 Instagram.configure do |config|
     config.client_id = "8d0c4adf568146c7968559a9a7ee4d2f"
-    config.access_token = "80a93683b3d84490944f0631f910ff77"
+    config.access_token = "630194682.8d0c4ad.c79f81f6f10d43d8867a5c42c90d0d43"
 end
+
